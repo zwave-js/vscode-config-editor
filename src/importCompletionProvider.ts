0d642d6f9bcc881b46430b83e54e3125c9304b14
Dominic Griesel <dominic.griesel@nabucasa.com>
2023-01-19 22:57:56 +0100

    feat: find and suggest nearby manufacturer templates

--- src/importCompletionProvider.ts
+++ src/importCompletionProvider.ts
@@ -1,20 +1,27 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import { posix as path } from "path";
 import * as vscode from "vscode";
 import { LanguageService as JsonLanguageService } from "vscode-json-languageservice";
+import { readJsonWithTemplate } from "./JsonTemplate";
 import {
 	formatTemplateDefinition,
 	getConfigFileDocumentSelector,
 	nodeIsPropertyName,
 	nodeIsPropertyValue,
 	parseImportSpecifier,
-	readJSON,
 	resolveTemplateFile,
 } from "./shared";
 
+const makeTemplateImportSpecifier = (filePath: string) => `${filePath}#\${0}`;
+const makeTemplateImport = (filePath: string) =>
+	`\\$import": "${makeTemplateImportSpecifier(filePath)}",`;
+
 const masterTemplateImportPath = `~/templates/master_template.json`;
-const masterTemplateImportSpecifier = `${masterTemplateImportPath}#\${0}`;
-const masterTemplateImport = `\\$import": "${masterTemplateImportSpecifier}",`;
+const masterTemplateImportSpecifier = makeTemplateImportSpecifier(
+	masterTemplateImportPath,
+);
+const masterTemplateImport = makeTemplateImport(masterTemplateImportPath);
 
 export function register(
 	workspace: vscode.WorkspaceFolder,
@@ -33,7 +40,13 @@ export function register(
 				);
 
 				if (nodeIsPropertyName(node)) {
+					const range = new vscode.Range(
+						document.positionAt(node.offset + 1), // start inside the quotes
+						document.positionAt(node.offset + node.length), // include the final quotes in the completion
+					);
+
 					// We're in a property name, suggest importing from the master template
+
 					const importMasterTemplate = new vscode.CompletionItem(
 						"Import from master template",
 						vscode.CompletionItemKind.Folder,
@@ -43,10 +56,7 @@ export function register(
 						masterTemplateImport,
 					);
 
-					importMasterTemplate.range = new vscode.Range(
-						document.positionAt(node.offset + 1), // start inside the quotes
-						document.positionAt(node.offset + node.length), // include the final quotes in the completion
-					);
+					importMasterTemplate.range = range;
 
 					// Trigger completions again, so an import can be chosen from the selected file
 					importMasterTemplate.command = {
@@ -59,6 +69,47 @@ export function register(
 					importMasterTemplate.filterText = "$import";
 
 					ret.push(importMasterTemplate);
+
+					//
+					// Also find nearby manufacturer templates and suggest those
+
+					const mfTemplates = await vscode.workspace.findFiles(
+						new vscode.RelativePattern(
+							vscode.Uri.joinPath(document.uri, ".."),
+							"templates/*.json",
+						),
+					);
+
+					for (const mfTmpl of mfTemplates) {
+						try {
+							const importMfTmpl = new vscode.CompletionItem(
+								"Import from manufacturer template",
+								vscode.CompletionItemKind.Folder,
+							);
+							// Using a snippet string allows us to place the cursor inside the completion
+							const relativePath = path.relative(
+								path.dirname(document.uri.fsPath),
+								mfTmpl.fsPath,
+							);
+							importMfTmpl.insertText = new vscode.SnippetString(
+								makeTemplateImport(relativePath),
+							);
+							importMfTmpl.detail = relativePath;
+							importMfTmpl.filterText = "$import";
+
+							importMfTmpl.range = range;
+
+							// Trigger completions again, so an import can be chosen from the selected file
+							importMfTmpl.command = {
+								command: "editor.action.triggerSuggest",
+								title: "Re-trigger completions...",
+							};
+
+							ret.push(importMfTmpl);
+						} catch (e) {
+							console.error(e);
+						}
+					}
 				} else if (
 					nodeIsPropertyValue(node) &&
 					node.parent.keyNode.value === "$import"
@@ -67,6 +118,11 @@ export function register(
 					const currentValue = node.value as string;
 					const spec = parseImportSpecifier(currentValue);
 					if (!spec) {
+						const range = new vscode.Range(
+							document.positionAt(node.offset + 1), // start inside the quotes
+							document.positionAt(node.offset + node.length - 1),
+						);
+
 						// Suggest the master template
 						const masterTemplate = new vscode.CompletionItem(
 							"Master template",
@@ -80,10 +136,7 @@ export function register(
 						masterTemplate.filterText = masterTemplateImportPath;
 						masterTemplate.detail = masterTemplateImportPath;
 
-						masterTemplate.range = new vscode.Range(
-							document.positionAt(node.offset + 1), // start inside the quotes
-							document.positionAt(node.offset + node.length - 1),
-						);
+						masterTemplate.range = range;
 
 						// Trigger completions again, so an import can be chosen from the selected file
 						masterTemplate.command = {
@@ -92,55 +145,111 @@ export function register(
 						};
 
 						ret.push(masterTemplate);
+
+						//
+						// Also suggest nearby manufacturer templates
+
+						const mfTemplates = await vscode.workspace.findFiles(
+							new vscode.RelativePattern(
+								vscode.Uri.joinPath(document.uri, ".."),
+								"templates/*.json",
+							),
+						);
+
+						for (const mfTmpl of mfTemplates) {
+							try {
+								const importMfTmpl = new vscode.CompletionItem(
+									"Manufacturer template",
+									vscode.CompletionItemKind.Folder,
+								);
+								// Using a snippet string allows us to place the cursor inside the completion
+								const relativePath = path.relative(
+									path.dirname(document.uri.fsPath),
+									mfTmpl.fsPath,
+								);
+								importMfTmpl.insertText =
+									new vscode.SnippetString(
+										makeTemplateImportSpecifier(
+											relativePath,
+										),
+									);
+								importMfTmpl.sortText = relativePath;
+								importMfTmpl.filterText = relativePath;
+								importMfTmpl.detail = relativePath;
+
+								importMfTmpl.range = range;
+
+								// Trigger completions again, so an import can be chosen from the selected file
+								importMfTmpl.command = {
+									command: "editor.action.triggerSuggest",
+									title: "Re-trigger completions...",
+								};
+
+								ret.push(importMfTmpl);
+							} catch (e) {
+								console.error(e);
+							}
+						}
 					} else {
 						// Suggest valid templates from the selected file
-						const uri = resolveTemplateFile(
-							workspace,
-							document.uri,
-							spec.filename,
-						);
-						const fileContent = await readJSON(uri);
-
-						const importSuggestions = Object.entries<
-							Record<string, any>
-						>(fileContent).map(
-							([
-								key,
-								{
-									$label: label = key,
-									$description,
-									...$import
-								},
-							]) => {
-								let documentation = formatTemplateDefinition(
-									$import,
-									undefined,
-									$description,
+						try {
+							const uri = resolveTemplateFile(
+								workspace,
+								document.uri,
+								spec.filename,
+							);
+							const fileContent: Record<string, any> =
+								await readJsonWithTemplate(
+									workspace,
+									uri.fsPath,
 								);
 
-								const completionItem: vscode.CompletionItem =
-									new vscode.CompletionItem(
-										key,
-										vscode.CompletionItemKind.Snippet,
+							const importSuggestions = Object.entries<
+								Record<string, any>
+							>(fileContent).map(
+								([
+									key,
+									{
+										$label: label = key,
+										$description,
+										...$import
+									},
+								]) => {
+									let documentation =
+										formatTemplateDefinition(
+											$import,
+											undefined,
+											$description,
+										);
+
+									const completionItem: vscode.CompletionItem =
+										new vscode.CompletionItem(
+											key,
+											vscode.CompletionItemKind.Snippet,
+										);
+									completionItem.detail = label;
+									// completionItem.sortText = key;
+									// completionItem.insertText = key;
+									// completionItem.filterText = key;
+									completionItem.documentation =
+										new vscode.MarkdownString(
+											documentation,
+										);
+									completionItem.range = new vscode.Range(
+										position.translate(
+											0,
+											-spec.templateKey.length,
+										),
+										position,
 									);
-								completionItem.detail = label;
-								// completionItem.sortText = key;
-								// completionItem.insertText = key;
-								// completionItem.filterText = key;
-								completionItem.documentation =
-									new vscode.MarkdownString(documentation);
-								completionItem.range = new vscode.Range(
-									position.translate(
-										0,
-										-spec.templateKey.length,
-									),
-									position,
-								);
-								return completionItem;
-							},
-						);
+									return completionItem;
+								},
+							);
 
-						ret.push(...importSuggestions);
+							ret.push(...importSuggestions);
+						} catch (e) {
+							console.error(e);
+						}
 					}
 				}
 
